54a9f5f36c5ebbfc20066b9144b6e866622fb138
jhkim593 <fpdlwjzlr@naver.com>
2023-06-30 03:07:16 +0900

    cloud config

---
+++ spring_cloud/cloud-config/config-client/user-service/src/main/java/com/cloud/user/UserController.java
@@ -0,0 +1,31 @@
+package com.cloud.user;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class UserController {
+
+    @Value("${user.var1}")
+    private String var1;
+
+    @Value("${user.var2}")
+    private String var2;
+
+    @Value("${common.var}")
+    private String commonVar1;
+
+    @GetMapping("/test")
+    public ResponseEntity test() {
+        String response =
+                "var1 : " + var1+ " , "+
+                "var2 : " + var2 + " , " +
+                "common.var : "+commonVar1;
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+}
