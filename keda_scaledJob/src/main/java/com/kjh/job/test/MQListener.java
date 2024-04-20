98bf0c72b1fbe8c318d68b9684e4472be923caec
jhkim593 <fpdlwjzlr@naver.com>
2024-04-20 14:20:22 +0900

    scaledJob

--- keda_scaledJob/src/main/java/com/kjh/job/test/MQListener.java
+++ keda_scaledJob/src/main/java/com/kjh/job/test/MQListener.java
@@ -1,29 +1,44 @@
 package com.kjh.job.test;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 @Getter
 @Setter
 public class MQListener {
-    private static final String QUEUE_NAME = "queue";
+    private static final String QUEUE_NAME = "a";
+    private final RabbitTemplate rabbitTemplate;
     private Status status = Status.IDLE;
-
 
     public enum Status {
         ING, IDLE
     }
 
-    @RabbitListener(queues = QUEUE_NAME)
-    public void getCommandMessage(Message<String> message) {
+    @Scheduled(fixedDelay = 5000)
+    public void getMessage() { if(status.equals(Status.ING)) return;
+        Object message = rabbitTemplate.receiveAndConvert(QUEUE_NAME);
+        if(message == null) return;
         this.setStatus(Status.ING);
 
         while (true){
-            if(status.name().equals(Status.IDLE)) break;
+            if(status.equals(Status.IDLE)) {
+                break;
+            }
         }
     }
 }
