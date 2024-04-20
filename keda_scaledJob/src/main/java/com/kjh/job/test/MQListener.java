c919e533f7ef9a324a8a9797f5aea6eaace543d0
jhkim593 <fpdlwjzlr@naver.com>
2024-04-20 18:38:44 +0900

    scaledJob

--- keda_scaledJob/src/main/java/com/kjh/job/test/MQListener.java
+++ keda_scaledJob/src/main/java/com/kjh/job/test/MQListener.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -20,25 +21,22 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Getter
 @Setter
+@Slf4j
 public class MQListener {
     private static final String QUEUE_NAME = "a";
     private final RabbitTemplate rabbitTemplate;
     private Status status = Status.IDLE;
 
     public enum Status {
-        ING, IDLE
+        ING, IDLE, COMPLETED
     }
 
     @Scheduled(fixedDelay = 5000)
-    public void getMessage() { if(status.equals(Status.ING)) return;
+    public void getMessage() {
+        if(!status.equals(Status.IDLE)) return;
+        log.info("job start !");
         Object message = rabbitTemplate.receiveAndConvert(QUEUE_NAME);
         if(message == null) return;
         this.setStatus(Status.ING);
-
-        while (true){
-            if(status.equals(Status.IDLE)) {
-                break;
-            }
-        }
     }
 }
--- keda_scaledJob/src/main/java/com/kjh/job/test/TestController.java
+++ keda_scaledJob/src/main/java/com/kjh/job/test/TestController.java
@@ -1,6 +1,7 @@
 package com.kjh.job.test;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -8,12 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class TestController {
     private final MQListener mqListener;
 
-    @GetMapping("/api/status")
-    public ResponseEntity status(){
-        mqListener.setStatus(MQListener.Status.IDLE);
+    @GetMapping("/end/status")
+    public ResponseEntity endStatus(){
+        log.info("job end !");
+        mqListener.setStatus(MQListener.Status.COMPLETED);
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/get/Status")
+    public ResponseEntity status(){
+        return new ResponseEntity(mqListener.getStatus(),HttpStatus.OK);
     }
 }
